513f2cdc5c24a0b020c4d67b3de504cd56ab19b3
Mirsaid Abdullaev <abdullaevm017@gmail.com>
2024-06-19 13:12:49 +0100

    Add project files.

---
+++ IOReader.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace NeuralNetwork
+{
+    internal static class IOReader
+    {
+        static readonly string projectDirectory = "D:\\NeuralNetwork";
+
+        private static double[] CreateOneHot(byte Label)
+        {
+            double[] Result = null;
+            switch (Label)
+            {
+                case 0:
+                    Result = new double[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                    break;
+                case 1:
+                    Result = new double[] { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+                    break;
+                case 2:
+                    Result = new double[] { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+                    break;
+                case 3:
+                    Result = new double[] { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 };
+                    break;
+                case 4:
+                    Result = new double[] { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+                    break;
+                case 5:
+                    Result = new double[] { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 };
+                    break;
+                case 6:
+                    Result = new double[] { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 };
+                    break;
+                case 7:
+                    Result = new double[] { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 };
+                    break;
+                case 8:
+                    Result = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 };
+                    break;
+                case 9:
+                    Result = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+                    break;
+            }
+
+            return Result;
+        }
+        public static double[][] GetTestDataOutputs(int max = 0) //array of 10,000 image labels stored sequentially in one-hot encoding
+        {
+            double[][] Outputs;
+
+
+            using (FileStream ifsLabels = new FileStream(projectDirectory + @"\MNIST\t10k-labels.idx1-ubyte", FileMode.Open))
+            {
+                using BinaryReader brLabels = new BinaryReader(ifsLabels);
+                brLabels.ReadBigInt32(); //magic int, not required
+                int numLabels = brLabels.ReadBigInt32();
+                if (max != 0)
+                {
+                    numLabels = max;
+                }
+
+                Outputs = new double[numLabels][];
+
+                for (int i = 0; i < numLabels; i++)
+                {
+                    Outputs[i] = CreateOneHot(brLabels.ReadByte());
+                }
+            }
+            return Outputs;
+        }
+
+        public static double[][] GetTrainingDataOutputs(int max = 0)
+        {
+            double[][] Outputs;
+
+            using (FileStream ifsLabels = new FileStream(projectDirectory + @"\MNIST\train-labels.idx1-ubyte", FileMode.Open))
+            {
+                using BinaryReader brLabels = new BinaryReader(ifsLabels);
+                brLabels.ReadBigInt32(); //magic int, no purpose
+                int numLabels = brLabels.ReadBigInt32();
+                if (max != 0)
+                {
+                    numLabels = max;
+                }
+
+                Outputs = new double[numLabels][];
+
+                for (int i = 0; i < numLabels; i++)
+                {
+                    Outputs[i] = CreateOneHot(brLabels.ReadByte());
+                }
+            }
+
+            return Outputs;
+        }
+        public static double[][] GetTrainingDataInputs(int max = 0)
+        {
+            double[][] Outputs;
+
+            using (FileStream ifsImages = new FileStream(projectDirectory + @"\MNIST\train-images.idx3-ubyte", FileMode.Open))
+            {
+                using BinaryReader brImages = new BinaryReader(ifsImages);
+                brImages.ReadBigInt32(); //a magic number int, dont need this
+                int numImages = brImages.ReadBigInt32();
+                brImages.ReadBigInt32(); //num of rows, dont need this
+                brImages.ReadBigInt32(); //nu of cols, dont need this
+                if (max != 0)
+                {
+                    numImages = max;
+                }
+                Outputs = new double[numImages][];
+
+                for (int i = 0; i < numImages; i++)
+                {
+                    Outputs[i] = new double[784];
+                    for (int j = 0; j < 784; j++)
+                    {
+                        Outputs[i][j] = (double)brImages.ReadByte() / 255;
+                    }
+                }
+            }
+            return Outputs; //outputs stores images in sequential order like [image1, image2, image3...] where image = [0, 1, 2, ..., 783] each pixel value
+        }
+
+
+        public static double[][] GetTestDataInputs(int max = 0)
+        {
+            double[][] Outputs; //each row is an image
+
+            using (FileStream ifsImages = new FileStream(projectDirectory + @"\MNIST\t10k-images.idx3-ubyte", FileMode.Open))
+            {
+                using BinaryReader brImages = new BinaryReader(ifsImages);
+                brImages.ReadBigInt32(); //a magic number int, dont need this
+                int numImages = brImages.ReadBigInt32();
+                brImages.ReadBigInt32(); //num of rows, dont need this
+                brImages.ReadBigInt32(); //nu of cols, dont need this
+                if (max != 0)
+                {
+                    numImages = max;
+                }
+                Outputs = new double[numImages][];
+
+
+                for (int i = 0; i < numImages; i++)
+                {
+                    Outputs[i] = new double[784];
+                    for (int j = 0; j < 784; j++)
+                    {
+                        Outputs[i][j] = (double)brImages.ReadByte() / 255;
+                    }
+                }
+            }
+
+            return Outputs;
+        }
+
+        public static NeuralNetwork LoadNetwork(string FilePath)
+        {
+            if (!File.Exists(FilePath))
+            {
+                throw new Exception("Error: no file found at the specified location.");
+            }
+            string[] NetData = ParseFile(FilePath);
+            int NoOfLayers = Convert.ToInt32(NetData[0]);
+            Layer[] Layers = new Layer[NoOfLayers];
+            string[] LayerData = NetData[1].Split(',');
+            Layers[0] = new Layer(Convert.ToInt32(LayerData[0]), 0);
+
+            //input layer has no prev neurons and no weights matrix or bias
+            for (int i = 2; i <= NoOfLayers; i++)
+            {
+                LayerData = NetData[i].Split(',');
+                int currNeurons = Convert.ToInt32(LayerData[0]);
+                int prevNeurons = Convert.ToInt32(LayerData[1]);
+
+                double[][] Weights = new double[prevNeurons][];
+                double[] Bias = new double[currNeurons];
+                int currRow = -1;
+                for (int j = 2; j < 2 + prevNeurons * currNeurons; j++)
+                {
+                    int col = (j - 2) % currNeurons;
+                    if (col == 0) //creating a new row
+                    {
+                        currRow++;
+                        Weights[currRow] = new double[currNeurons];
+                        Weights[currRow][col] = Convert.ToDouble(LayerData[j]);
+                    }
+                    else //on the same row
+                    {
+                        Weights[currRow][col] = Convert.ToDouble(LayerData[j]);
+                    }
+                }
+                for (int j = 2 + prevNeurons * currNeurons; j < LayerData.Length; j++)
+                {
+                    Bias[j - 2 - prevNeurons * currNeurons] = Convert.ToDouble(LayerData[j]);
+                }
+                Layer layer = new Layer(currNeurons, prevNeurons, Weights, Bias);
+                Layers[i - 1] = layer;
+            }
+            return new NeuralNetwork(Layers);
+        }
+        private static int ReadBigInt32(this BinaryReader br)
+        {
+            var bytes = br.ReadBytes(sizeof(int));
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+        private static string[] ParseFile(string FilePath) //returns all the rows of a csv file
+        {
+            string[] FullData;
+            using (StreamReader reader = new StreamReader(FilePath))
+            {
+                try
+                {
+                    FullData = reader.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return FullData; //returns all not-null data rows
+        }
+
+    }
+
+}
---
+++ NeuralNetwork.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace NeuralNetwork
+{
+    internal class NeuralNetwork
+    {
+        public Layer[] Layers;
+        public NeuralNetwork(int[] LayersData) //LayersData holds number of neurons in each layer in the network e.g. {2, 3, 1}, {100, 150, 50, 10, 8} etc.
+        {
+            Layers = new Layer[LayersData.Length];
+            Layers[0] = new Layer(LayersData[0], 0);
+            for (int i = 1; i < Layers.Length; i++)
+            {
+                Layers[i] = new Layer(LayersData[i], LayersData[i - 1]);
+            }
+        }
+
+        public NeuralNetwork(Layer[] Layers) //for loading from storage
+        {
+            this.Layers = Layers;
+        }
+
+        public void SaveNetwork(string FileName)
+        {
+            string FilePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\MNIST_Net\" + FileName + ".csv";
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\MNIST_Net\"))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\MNIST_Net\");
+            }
+            using StreamWriter sw = new StreamWriter(FilePath, false);
+            sw.Write(GetNetworkData());
+            sw.Flush();
+        }
+
+        private string GetNetworkData()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(Layers.Length.ToString());
+            for (int i = 0;i < Layers.Length - 1; i++)
+            {
+                stringBuilder.AppendLine(Layers[i].GetLayerData());
+            }
+            stringBuilder.Append(Layers[^1].GetLayerData());
+            return stringBuilder.ToString();
+        }
+    }
+    internal class Layer
+    {
+        public int NeuronCount; //number of neurons in the current layer
+        public int PrevNeuronCount; //number of neurons in prev layer
+
+        public readonly double[] Bias; //Each index relates to each neuron in this layer (b(n))
+        public readonly double[] Outputs; //As above, but each index is the same neuron's output value (a(n) = f(z(n)))
+        public readonly double[] dCdZ; //As above, but each index is the same neuron's dC/dZ value (dC/dZ(n) = dC/da(n) * da(n)/dz(n) = 2 * (a(n) - y) * f'(z(n)))
+        public readonly double[][] Weights;
+        public Layer(int Neurons, int PrevNeurons) //setting all weights and biases for all neurons in one go
+        {
+            NeuronCount = Neurons;
+            PrevNeuronCount = PrevNeurons;
+            if (PrevNeuronCount > 0) //checking not input layer
+            {
+                double temp = Math.Sqrt(PrevNeuronCount);
+                Weights = new double[PrevNeuronCount][];
+                Bias = new double[NeuronCount];
+                dCdZ = new double[NeuronCount];
+                for (int i = 0; i < PrevNeuronCount; i++)
+                {
+                    Weights[i] = new double[NeuronCount];
+                    for (int j = 0; j < NeuronCount; j++)
+                    {
+                        Weights[i][j] = GetRandom() / temp;
+                    }
+                }
+                for (int i = 0; i < NeuronCount; i++)
+                {
+                    Bias[i] = GetRandom() / temp;
+                }
+            }
+            Outputs = new double[NeuronCount]; //all layers have output layer, as input layer has a(0) which are just the network inputs
+        }
+        public Layer(int Neurons, int PrevNeurons, double[][] Weights, double[] Bias) //for loading a layer from storage
+        {
+            NeuronCount = Neurons;
+            PrevNeuronCount = PrevNeurons;
+            if(PrevNeuronCount > 0) //input layer
+            {
+                this.Weights = Weights;
+                this.Bias = Bias;
+                dCdZ = new double[NeuronCount];
+            }
+            Outputs = new double[NeuronCount];
+        }
+
+        static double GetRandom() //y = sqrt(-2 * ln(1 - x1)) * cos(2 * pi * x2), 0 < x1 <= 1, 0 < x2 <= 1
+        {
+            Random random = new Random();
+            return Math.Sqrt(-2 * Math.Log(1 - random.NextDouble())) * Math.Cos(2 * Math.PI * random.NextDouble());
+        }
+
+        public string GetLayerData()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(NeuronCount.ToString() + "," + PrevNeuronCount.ToString() + ",");
+            if (PrevNeuronCount != 0)
+            {
+                for (int i = 0; i < PrevNeuronCount; i++)
+                {
+                    stringBuilder.Append(string.Join(",", Weights[i]) + ",");
+                }
+                stringBuilder.Append(string.Join(",", Bias));
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
